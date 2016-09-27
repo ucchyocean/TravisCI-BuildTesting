5ebf04bc44d2e98173d0f6498955290006ada429
ucchy <ocean163@hotmail.com>
2016-09-27 10:48:38 +0900

    test

---
+++ src/main/java/com/github/ucchyocean/lc/command/ListCommand.java
@@ -0,0 +1,162 @@
+/*
+ * @author     ucchy
+ * @license    LGPLv3
+ * @copyright  Copyright ucchy 2013
+ */
+package com.github.ucchyocean.lc.command;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.github.ucchyocean.lc.Resources;
+import com.github.ucchyocean.lc.channel.Channel;
+import com.github.ucchyocean.lc.channel.ChannelPlayer;
+
+/**
+ * listコマンドの実行クラス
+ * @author ucchy
+ */
+public class ListCommand extends SubCommandAbst {
+
+    private static final String LIST_FIRSTLINE = Resources.get("listFirstLine");
+    private static final String LIST_ENDLINE = Resources.get("listEndLine");
+    private static final String LIST_FORMAT = Resources.get("listFormat");
+
+    private static final String COMMAND_NAME = "list";
+    private static final String PERMISSION_NODE = "lunachat." + COMMAND_NAME;
+    private static final String USAGE_KEY = "usageList";
+
+    /**
+     * コマンドを取得します。
+     * @return コマンド
+     * @see com.github.ucchyocean.lc.command.SubCommandAbst#getCommandName()
+     */
+    @Override
+    public String getCommandName() {
+        return COMMAND_NAME;
+    }
+
+    /**
+     * パーミッションノードを取得します。
+     * @return パーミッションノード
+     * @see com.github.ucchyocean.lc.command.SubCommandAbst#getPermissionNode()
+     */
+    @Override
+    public String getPermissionNode() {
+        return PERMISSION_NODE;
+    }
+
+    /**
+     * コマンドの種別を取得します。
+     * @return コマンド種別
+     * @see com.github.ucchyocean.lc.command.SubCommandAbst#getCommandType()
+     */
+    @Override
+    public CommandType getCommandType() {
+        return CommandType.USER;
+    }
+
+    /**
+     * 使用方法に関するメッセージをsenderに送信します。
+     * @param sender コマンド実行者
+     * @param label 実行ラベル
+     * @see com.github.ucchyocean.lc.command.SubCommandAbst#sendUsageMessage()
+     */
+    @Override
+    public void sendUsageMessage(
+            CommandSender sender, String label) {
+        sendResourceMessage(sender, "", USAGE_KEY, label);
+    }
+
+    /**
+     * コマンドを実行します。
+     * @param sender コマンド実行者
+     * @param label 実行ラベル
+     * @param args 実行時の引数
+     * @return コマンドが実行されたかどうか
+     * @see com.github.ucchyocean.lc.command.SubCommandAbst#runCommand(java.lang.String[])
+     */
+    @Override
+    public boolean runCommand(
+            CommandSender sender, String label, String[] args) {
+
+        Player player = null;
+        if (sender instanceof Player) {
+            player = (Player) sender;
+        }
+
+        // リストを取得して表示する
+        ArrayList<String> list = getList(player);
+        for (String msg : list) {
+            sender.sendMessage(msg);
+        }
+        return true;
+    }
+
+    /**
+     * リスト表示用のリストを返す
+     * @param player プレイヤー、指定しない場合はnullにする
+     * @return リスト
+     */
+    private ArrayList<String> getList(Player player) {
+
+        ArrayList<String> items = new ArrayList<String>();
+        String dchannel = "";
+        String playerName = "";
+        if ( player != null ) {
+            playerName = player.getName();
+            Channel def = api.getDefaultChannel(playerName);
+            if ( def == null ) {
+                dchannel = "";
+            } else {
+                dchannel = def.getName();
+            }
+        }
+        ChannelPlayer cp = ChannelPlayer.getChannelPlayer(player);
+
+        items.add(LIST_FIRSTLINE);
+        for ( Channel channel : api.getChannels() ) {
+
+            // BANされているチャンネルは表示しない
+            if ( channel.getBanned().contains(cp) ) {
+                continue;
+            }
+
+            // 個人チャットはリストに表示しない
+            if ( channel.isPersonalChat() ) {
+                continue;
+            }
+
+            // デフォルト発言先なら赤に、非表示中なら暗青にする。
+            String disp = ChatColor.WHITE + channel.getName();
+            if ( channel.getName().equalsIgnoreCase(dchannel) ) {
+                disp = ChatColor.RED + channel.getName();
+            } else if ( channel.getHided().contains(cp) ) {
+                disp = ChatColor.DARK_AQUA + channel.getName();
+            }
+
+            if ( player != null &&
+                    !channel.getMembers().contains(cp) &&
+                    !channel.isGlobalChannel() ) {
+
+                // 未参加で visible=false のチャンネルは表示しない
+                if ( !channel.isVisible() ) {
+                    continue;
+                }
+                disp = ChatColor.GRAY + channel.getName();
+            }
+            String desc = channel.getDescription();
+            int onlineNum = channel.getOnlineNum();
+            int memberNum = channel.getTotalNum();
+            String item = String.format(
+                    LIST_FORMAT, disp, onlineNum, memberNum, desc);
+            items.add(item);
+        }
+        items.add(LIST_ENDLINE);
+
+        return items;
+    }
+}
---
+++ src/main/java/com/github/ucchyocean/lc/command/UnhideCommand.java
@@ -0,0 +1,145 @@
+/*
+ * @author     ucchy
+ * @license    LGPLv3
+ * @copyright  Copyright ucchy 2013
+ */
+package com.github.ucchyocean.lc.command;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.github.ucchyocean.lc.Utility;
+import com.github.ucchyocean.lc.channel.Channel;
+import com.github.ucchyocean.lc.channel.ChannelPlayer;
+
+/**
+ * unhideコマンドの実行クラス
+ * @author ucchy
+ */
+public class UnhideCommand extends SubCommandAbst {
+
+    private static final String COMMAND_NAME = "unhide";
+    private static final String PERMISSION_NODE = "lunachat." + COMMAND_NAME;
+    private static final String USAGE_KEY1 = "usageUnhide";
+    private static final String USAGE_KEY2 = "usageUnhidePlayer";
+
+    /**
+     * コマンドを取得します。
+     * @return コマンド
+     * @see com.github.ucchyocean.lc.command.SubCommandAbst#getCommandName()
+     */
+    @Override
+    public String getCommandName() {
+        return COMMAND_NAME;
+    }
+
+    /**
+     * パーミッションノードを取得します。
+     * @return パーミッションノード
+     * @see com.github.ucchyocean.lc.command.SubCommandAbst#getPermissionNode()
+     */
+    @Override
+    public String getPermissionNode() {
+        return PERMISSION_NODE;
+    }
+
+    /**
+     * コマンドの種別を取得します。
+     * @return コマンド種別
+     * @see com.github.ucchyocean.lc.command.SubCommandAbst#getCommandType()
+     */
+    @Override
+    public CommandType getCommandType() {
+        return CommandType.USER;
+    }
+
+    /**
+     * 使用方法に関するメッセージをsenderに送信します。
+     * @param sender コマンド実行者
+     * @param label 実行ラベル
+     * @see com.github.ucchyocean.lc.command.SubCommandAbst#sendUsageMessage()
+     */
+    @Override
+    public void sendUsageMessage(
+            CommandSender sender, String label) {
+        sendResourceMessage(sender, "", USAGE_KEY1, label);
+        sendResourceMessage(sender, "", USAGE_KEY2, label);
+    }
+
+    /**
+     * コマンドを実行します。
+     * @param sender コマンド実行者
+     * @param label 実行ラベル
+     * @param args 実行時の引数
+     * @return コマンドが実行されたかどうか
+     * @see com.github.ucchyocean.lc.command.SubCommandAbst#runCommand(java.lang.String[])
+     */
+    @Override
+    public boolean runCommand(
+            CommandSender sender, String label, String[] args) {
+
+        // プレイヤーでなければ終了する
+        if (!(sender instanceof Player)) {
+            sendResourceMessage(sender, PREERR, "errmsgIngame");
+            return true;
+        }
+        ChannelPlayer player = ChannelPlayer.getChannelPlayer(sender);
+
+        // 引数チェック
+        String cname = null;
+        if ( args.length <= 1 ) {
+            Channel def = api.getDefaultChannel(player.getName());
+            if ( def != null ) {
+                cname = def.getName();
+            }
+        } else if ( args.length >= 2 ) {
+            cname = args[1];
+        } else {
+            sendResourceMessage(sender, PREERR, "errmsgCommand");
+            return true;
+        }
+
+        // チャンネルかプレイヤーが存在するかどうかをチェックする
+        boolean isChannelCommand = false;
+        Channel channel = api.getChannel(cname);
+        if ( channel != null ) {
+            isChannelCommand = true;
+        } else if ( Utility.getOfflinePlayer(cname) == null ) {
+            sendResourceMessage(sender, PREERR, "errmsgNotExistChannelAndPlayer");
+            return true;
+        }
+
+        if ( isChannelCommand ) {
+            // チャンネルが対象の場合の処理
+
+            // 非表示になっているかどうかをチェックする
+            if ( !channel.getHided().contains(player) ) {
+                sendResourceMessage(sender, PREERR, "errmsgAlreadyUnhided");
+                return true;
+            }
+
+            // 設定する
+            channel.getHided().remove(player);
+            channel.save();
+            sendResourceMessage(sender, PREINFO, "cmdmsgUnhided", channel.getName());
+
+            return true;
+
+        } else {
+            // プレイヤーが対象の場合の処理
+
+            // 既に表示になっていないかどうかをチェックする
+            ChannelPlayer hided = ChannelPlayer.getChannelPlayer(cname);
+            if ( !api.getHidelist(hided).contains(player) ) {
+                sendResourceMessage(sender, PREERR, "errmsgAlreadyUnhidedPlayer");
+                return true;
+            }
+
+            // 設定する
+            api.removeHidelist(player, hided);
+            sendResourceMessage(sender, PREINFO, "cmdmsgUnhidedPlayer", hided.getDisplayName());
+
+            return true;
+        }
+    }
+}
